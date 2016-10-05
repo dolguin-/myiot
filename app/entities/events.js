1431bd50cce91d6c9b798e7e1afc7e4388f0970b
Damian Olguin <damian@dolguin.com.ar>
2016-10-05 00:53:01 -0300

    remove comments

--- app/entities/events.js
+++ app/entities/events.js
@@ -1,16 +1,5 @@
 
 module.exports = Events;
-
-// function Events(params) {
-//   this.table = 'eventos';
-//   this.fields = [
-//     'id',
-//     'device_id',
-//     'dateTime',
-//     'duracion',
-//     'message'
-//   ];
-// };
 	
 function Events(params) {
   this.id = params['id'];
